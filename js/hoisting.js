9afe69bd6b48b85c194b1ac7a945d106441e6885
srikanthpragada <srikanthpragada@gmail.com>
2022-03-19 20:56:12 +0530

    19-MAR

---
+++ js/hoisting.js
@@ -0,0 +1,11 @@
+"use strict"
+v = 10
+console.log(v)
+print("Hello!")
+
+// print and v are hoisted 
+function print(msg) {
+    console.log(msg)
+}
+
+var v;
---
+++ js/operators.js
@@ -0,0 +1,9 @@
+
+console.log(10 ** 2)
+console.log(10 % 3)
+
+var a = 10
+var b = "10"
+
+console.log(a == b)
+console.log(a === b)
