470c29a8480c3313b6c0a2a86663480c76d7e135
srikanthpragada <srikanthpragada@gmail.com>
2022-03-21 20:56:30 +0530

    21-MAR

---
+++ js/stringdemo.js
@@ -0,0 +1,15 @@
+var st = "Hello"
+
+console.log(st.length)
+console.log(st[0])
+console.log(st.indexOf('l'))
+console.log(st.indexOf('a'))
+console.log(st.includes('a'))
+
+console.log( "abc,xyz,pqr".split(","))
+
+var c  = "A"
+console.log(c.charCodeAt(0))
+
+
+
