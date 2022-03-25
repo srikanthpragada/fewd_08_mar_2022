f9b55aaa453cd66ef1b58afe5b2d14b712c38f6f
srikanthpragada <srikanthpragada@gmail.com>
2022-03-25 20:57:21 +0530

    25-MAR

---
+++ js/destructuring.js
@@ -0,0 +1,18 @@
+let a = [10,20]
+
+let v1 = a[0]
+let v2 = a[1]
+
+//  Destructuring assignment
+let [n1, n2] = a
+
+console.log(n1)
+console.log(n2)
+
+let names = ['a','b','c']
+
+let [na1, ...na2 ] = names 
+
+console.log(na1)
+console.log(na2)
+
