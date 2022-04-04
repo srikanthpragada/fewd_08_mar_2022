a778911449db59ae0ee41887a1591568884bd876
srikanthpragada <srikanthpragada@gmail.com>
2022-04-04 20:59:41 +0530

    04-APR

---
+++ react/demo/src/basics/Hello.js
@@ -0,0 +1,12 @@
+
+// Function component 
+export default function Hello() {
+    var name = "Srikanth"
+    // code
+    return (
+        <div>
+            <h1 className="banner">Hello {name} </h1>
+            <h2>Good Morning!!</h2>
+        </div>
+    )
+}
